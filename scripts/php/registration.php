b633dec0ddd713c41e8b124b72883ec46f619c04
Eli Hansen <elihansen@yahoo.com>
2024-10-27 19:02:53 -0600

    edits

--- scripts/php/registration.php
+++ scripts/php/registration.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Establish database connection
 $conn = new mysqli('107.180.118.249', 'EliHansen', 'Bri@rwood2()', 'idea_factory');
