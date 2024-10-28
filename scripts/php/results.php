8ee56e0ddaf6a2f150f0f8bdf423129d3a196516
Eli Hansen <elihansen@yahoo.com>
2024-10-28 15:52:23 -0600

    edits

--- scripts/php/results.php
+++ scripts/php/results.php
@@ -50,7 +50,7 @@
     }
 
     if (isset($_SESSION['first_name']) && isset($_SESSION['selected_class'])) {
-        echo "<p>" . htmlspecialchars($_SESSION['first_name']) . " has been successfully registered for " . htmlspecialchars($_SESSION['selected_class']) . ".</p>";
+        echo "<h3>" . htmlspecialchars($_SESSION['first_name']) . " has been successfully registered for " . htmlspecialchars($_SESSION['selected_class']) . ".</h3>";
         unset($_SESSION['first_name']);
         unset($_SESSION['selected_class']);
     }
