83ad7b755cc7bb2cb256d0d365609b117fa1413a
Eli Hansen <elihansen@yahoo.com>
2024-11-07 13:29:29 -0700

    edited the cost to $150

--- scripts/php/payment.php
+++ scripts/php/payment.php
@@ -47,7 +47,7 @@ try {
 
     // Create Price for $200 with recurring billing
     $price = $stripe->prices->create([
-        'unit_amount' => 16000, // $200 in cents
+        'unit_amount' => 15000, // $200 in cents
         'currency' => 'usd',
         'recurring' => ['interval' => 'month'],
         'product' => $product->id,
